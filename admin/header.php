fb7a5be57221411fa5af1ebb658afa2c607756b1
hageduri <57488023+hageduri@users.noreply.github.com>
2019-11-23 01:08:31 +0530

    del

--- admin/header.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE HTML>
-<html lan="en_US">
-<head>
-	<meta charset = "UTF-8">
-	<title>Student Management System</title>
-	<link href="../css/style.css" rel="stylesheet" type=""text/css">
-</head>
-<body bgcolor="#FFFF33">
